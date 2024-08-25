d9f4581dd1703d27b6db8fcb6557fc70b5e02aea
Aleksey Solomatin <2717070@list.ru>
2024-08-25 22:32:29 +0300

    add auto test

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -1,23 +1,22 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashBackHackerTest {
 
     @Test
-    public void testRemain1() {
+    public void remain900() {
         CashBackHacker service = new CashBackHacker();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
         Assert.assertEquals(expected, actual);
     }
-
     @Test
-    public void testRemain2() {
+    public void remain0() {
         CashBackHacker service = new CashBackHacker();
         int amount = 1000;
         int actual = service.remain(amount);
